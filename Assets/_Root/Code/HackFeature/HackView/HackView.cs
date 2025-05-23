85cfa7f24c6a1f305ceb553e8623677898946ef1
w1n41 <dima200.dkv@gmail.com>
2025-05-23 13:12:15 +0500

    Started writing hack feature

---
+++ Assets/_Root/Code/HackFeature/HackView/HackView.cs
@@ -0,0 +1,25 @@
+﻿using _Root.Code.HackFeature.HackModel;
+using TMPro;
+
+namespace _Root.Code.HackFeature.HackView
+{
+    public class HackView : IHackView
+    {
+        public TextMeshProUGUI StatusText;
+        
+        public void ShowSuccess(int current, int required)
+        {
+            StatusText.text = $"Успех! {current}/{required}";
+        }
+
+        public void ShowFailure()
+        {
+            StatusText.text = "Неудача.";
+        }
+
+        public void ShowHackComplete()
+        {
+            StatusText.text = "Успешно взломано!";
+        }
+    }
+}
---
+++ Assets/_Root/Code/HackFeature/HackView/IHackView.cs
@@ -0,0 +1,11 @@
+﻿namespace _Root.Code.HackFeature.HackModel
+{
+    public interface IHackView
+    {
+        public void ShowSuccess(int current, int required);
+        
+        public void ShowFailure();
+        
+        public void ShowHackComplete();
+    }
+}
