1c690c57916e16161a017bf5b6758c10dd465729
Nakohartum <58501792+Nakohartum@users.noreply.github.com>
2025-05-23 11:14:23 +0500

    Added updatable

---
+++ Assets/_Root/Code/UpdateFeature/UpdateManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Root.Code.UpdateFeature
+{
+    public class UpdateManager : IUpdatable
+    {
+        private List<IUpdatable> _updatables = new List<IUpdatable>();
+
+        public void AddUpdatable(IUpdatable updatable)
+        {
+            _updatables.Add(updatable);
+        }
+
+        public void RemoveUpdatable(IUpdatable updatable)
+        {
+            _updatables.Remove(updatable);
+        }
+        
+        public void Update(float deltaTime)
+        {
+            foreach (IUpdatable updatable in _updatables)
+            {
+                updatable.Update(deltaTime);
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (IUpdatable updatable in _updatables)
+            {
+                updatable.Dispose();
+            }
+            _updatables.Clear();
+        }
+    }
+}
